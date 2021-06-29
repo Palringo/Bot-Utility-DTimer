15a37497020b87b842c216a2b3b78dc78d13ec2e
David Walters <david.walters@palringo.com>
2021-06-29 13:01:54 -0400

    initial commit

--- test/MultiNodes.js
+++ test/MultiNodes.js
@@ -1,78 +1,79 @@
-'use strict';
+/* eslint-disable */
 
-var DTimer = require('..').DTimer;
-var async = require('async');
-var assert = require('assert');
-var Promise = require('bluebird');
-var redis = Promise.promisifyAll(require("redis"));
+const DTimer = require('..').DTimer;
+const async = require('async');
+const assert = require('assert');
+const redis = require('ioredis');
 
-describe('Multiple nodes', function () {
-    var numNodes = 8;
-    var nodes;
+// TODO: Failing
+describe('Multiple nodes', () => {
+    const numNodes = 8;
+    let nodes;
 
-    before(function () {
+    before(() => {
     });
 
-    beforeEach(function (done) {
+    beforeEach((done) => {
         nodes = [];
         function prepareNode(id, cb) {
-            var node = {
-                id: id,
-                numRcvd: 0
+            const node = {
+                id,
+                numRcvd: 0,
             };
             async.series([
-                function (next) {
+                function(next) {
                     node.pub = redis.createClient();
                     node.pub.once('ready', next);
                 },
-                function (next) {
+                function(next) {
                     node.sub = redis.createClient();
                     node.sub.once('ready', next);
                 },
-                function (next) {
+                function(next) {
                     node.pub.select(9, next);
-                }
-            ], function (err) {
+                },
+            ], (err) => {
                 if (err) { return void(cb(err)); }
-                node.dt = new DTimer('ch' + id, node.pub, node.sub);
-                setTimeout(function () { cb(null, node); }, 100);
+                node.dt = new DTimer(`ch${ id}`, node.pub, node.sub);
+                // eslint-disable-next-line no-magic-numbers
+                setTimeout(() => { cb(null, node); }, 100);
             });
         }
 
         async.whilst(
-            function () { return (nodes.length < numNodes); },
-            function (next) {
-                prepareNode(nodes.length, function (err, node) {
+            () => (nodes.length < numNodes),
+            (next) => {
+
+                console.log('preparing nodes');
+                prepareNode(nodes.length, (err, node) => {
                     if (err) { return void(next(err)); }
                     nodes.push(node);
                     next();
                 });
             },
-            function (err) {
+            (err) => {
                 if (err) {
-                    return void(done(err));
+                    return done(err);
                 }
-                nodes[0].pub.flushdb(done);
-            }
+                nodes[0].pub.flushdb();
+                done();
+            },
         );
     });
 
-    afterEach(function () {
-        nodes.forEach(function (node) {
-            node.dt.removeAllListeners();
+    afterEach(() => {
+        nodes.forEach((node) => {
             node.dt = null;
-            node.pub.removeAllListeners();
             node.pub.end();
             node.pub = null;
-            node.sub.removeAllListeners();
             node.sub.end();
             node.sub = null;
         });
         nodes = [];
     });
 
-    it('MaxEvents 1 - each node receives 1 event', function (done) {
-        var evts = [
+    it('MaxEvents 1 - each node receives 1 event', (done) => {
+        const evts = [
             { msg: { msg: 'msg0' }, delay: 60 },
             { msg: { msg: 'msg1' }, delay: 60 },
             { msg: { msg: 'msg2' }, delay: 60 },
@@ -80,18 +81,19 @@ describe('Multiple nodes', function () {
             { msg: { msg: 'msg4' }, delay: 60 },
             { msg: { msg: 'msg5' }, delay: 60 },
             { msg: { msg: 'msg6' }, delay: 60 },
-            { msg: { msg: 'msg7' }, delay: 60 }
+            { msg: { msg: 'msg7' }, delay: 60 },
         ];
-        var numRcvd = 0;
+        let numRcvd = 0;
+
         // Set up each node to grab 1 event at a time.
-        nodes.forEach(function (node) {
+        nodes.forEach((node) => {
             node.dt.maxEvents = 1;
         });
         async.series([
-            function (next) {
-                var numJoined = 0;
-                nodes.forEach(function (node) {
-                    node.dt.join(function (err) {
+            function(next) {
+                let numJoined = 0;
+                nodes.forEach((node) => {
+                    node.dt.join((err) => {
                         if (err) { return void(next(err)); }
                         numJoined++;
                         if (numJoined === nodes.length) {
@@ -100,21 +102,21 @@ describe('Multiple nodes', function () {
                     });
                 });
             },
-            function (next) {
-                var since = Date.now();
-                evts.forEach(function (evt) {
-                    nodes[0].dt.post(evt.msg, evt.delay, function (err, evId) {
+            function(next) {
+                const since = Date.now();
+                evts.forEach((evt) => {
+                    nodes[0].dt.post(evt.msg, evt.delay, (err, evId) => {
                         assert.ifError(err);
                         evt.id = evId;
                         evt.postDelay = Date.now() - since;
                         evt.posted = true;
                     });
                 });
-                nodes.forEach(function (node) {
-                    node.dt.on('event', function (ev) {
+                nodes.forEach((node) => {
+                    node.dt.on('event', (ev) => {
                         node.numRcvd++;
-                        var elapsed = Date.now() - since;
-                        evts.forEach(function (evt) {
+                        const elapsed = Date.now() - since;
+                        evts.forEach((evt) => {
                             if (evt.msg.msg === ev.msg) {
                                 numRcvd++;
                                 evt.elapsed = elapsed;
@@ -127,43 +129,43 @@ describe('Multiple nodes', function () {
                 });
                 setTimeout(next, 100);
             },
-            function (next) {
-                nodes[0].pub.llen('dt:ch', function (err, reply) {
+            function(next) {
+                nodes[0].pub.llen('dt:ch', (err, reply) => {
                     assert.ifError(err);
                     assert.equal(reply, 8);
                     next();
                 });
             },
-            function (next) {
-                var numLeft = 0;
-                nodes.forEach(function (node) {
-                    node.dt.leave(function () {
+            function(next) {
+                let numLeft = 0;
+                nodes.forEach((node) => {
+                    node.dt.leave(() => {
                         numLeft ++;
                         if (numLeft === nodes.length) {
                             return next();
                         }
                     });
                 });
-            }
-        ], function (err, results) {
+            },
+        ], (err, results) => {
             void(results);
             assert.ifError(err);
-            evts.forEach(function (evt) {
+            evts.forEach((evt) => {
                 assert.ok(evt.posted);
                 assert.deepEqual(evt.msg.msg, evt.rcvd.msg);
                 assert(evt.elapsed < evt.delay + 200);
                 assert(evt.elapsed > evt.delay);
             });
             assert.equal(numRcvd, evts.length);
-            nodes.forEach(function (node) {
+            nodes.forEach((node) => {
                 assert.equal(node.numRcvd, 1);
             });
             done();
         });
     });
 
-    it('MaxEvents 2 - each node receives 0 or 2 events', function (done) {
-        var evts = [
+    it('MaxEvents 2 - each node receives 0 or 2 events', (done) => {
+        const evts = [
             { msg: { msg: 'msg0' }, delay: 60 },
             { msg: { msg: 'msg1' }, delay: 60 },
             { msg: { msg: 'msg2' }, delay: 60 },
@@ -171,18 +173,19 @@ describe('Multiple nodes', function () {
             { msg: { msg: 'msg4' }, delay: 60 },
             { msg: { msg: 'msg5' }, delay: 60 },
             { msg: { msg: 'msg6' }, delay: 60 },
-            { msg: { msg: 'msg7' }, delay: 60 }
+            { msg: { msg: 'msg7' }, delay: 60 },
         ];
-        var numRcvd = 0;
+        let numRcvd = 0;
+
         // Set up each node to grab 1 event at a time.
-        nodes.forEach(function (node) {
+        nodes.forEach((node) => {
             node.dt.maxEvents = 2;
         });
         async.series([
-            function (next) {
-                var numJoined = 0;
-                nodes.forEach(function (node) {
-                    node.dt.join(function (err) {
+            function(next) {
+                let numJoined = 0;
+                nodes.forEach((node) => {
+                    node.dt.join((err) => {
                         if (err) { return void(next(err)); }
                         numJoined++;
                         if (numJoined === nodes.length) {
@@ -191,21 +194,21 @@ describe('Multiple nodes', function () {
                     });
                 });
             },
-            function (next) {
-                var since = Date.now();
-                evts.forEach(function (evt) {
-                    nodes[0].dt.post(evt.msg, evt.delay, function (err, evId) {
+            function(next) {
+                const since = Date.now();
+                evts.forEach((evt) => {
+                    nodes[0].dt.post(evt.msg, evt.delay, (err, evId) => {
                         assert.ifError(err);
                         evt.id = evId;
                         evt.postDelay = Date.now() - since;
                         evt.posted = true;
                     });
                 });
-                nodes.forEach(function (node) {
-                    node.dt.on('event', function (ev) {
+                nodes.forEach((node) => {
+                    node.dt.on('event', (ev) => {
                         node.numRcvd++;
-                        var elapsed = Date.now() - since;
-                        evts.forEach(function (evt) {
+                        const elapsed = Date.now() - since;
+                        evts.forEach((evt) => {
                             if (evt.msg.msg === ev.msg) {
                                 numRcvd++;
                                 evt.elapsed = elapsed;
@@ -218,35 +221,35 @@ describe('Multiple nodes', function () {
                 });
                 setTimeout(next, 100);
             },
-            function (next) {
-                nodes[0].pub.llen('dt:ch', function (err, reply) {
+            function(next) {
+                nodes[0].pub.llen('dt:ch', (err, reply) => {
                     assert.ifError(err);
                     assert.equal(reply, 8);
                     next();
                 });
             },
-            function (next) {
-                var numLeft = 0;
-                nodes.forEach(function (node) {
-                    node.dt.leave(function () {
+            function(next) {
+                let numLeft = 0;
+                nodes.forEach((node) => {
+                    node.dt.leave(() => {
                         numLeft ++;
                         if (numLeft === nodes.length) {
                             return next();
                         }
                     });
                 });
-            }
-        ], function (err, results) {
+            },
+        ], (err, results) => {
             void(results);
             assert.ifError(err);
-            evts.forEach(function (evt) {
+            evts.forEach((evt) => {
                 assert.ok(evt.posted);
                 assert.deepEqual(evt.msg.msg, evt.rcvd.msg);
                 assert(evt.elapsed < evt.delay + 200);
                 assert(evt.elapsed > evt.delay);
             });
             assert.equal(numRcvd, evts.length);
-            nodes.forEach(function (node) {
+            nodes.forEach((node) => {
                 if (node.numRcvd > 0) {
                     assert.equal(node.numRcvd, 2);
                 }
